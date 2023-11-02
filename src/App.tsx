002eca6e5d35acc063578da5a8879dfc07d70d60
Abhinandhan <abhinandhan_kumaresh@thbs.com>
2023-11-02 16:06:55 +0530

    route guard

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import './App.scss';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import PrelimInfoForm from './preliminfoform';
 import BasicConfigForm from './basicconfigform';
 import TaskForm from './taskform';
@@ -14,17 +14,24 @@ function App() {
   const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
   const [activeTab, setActiveTab] = useState(0);
 
-
   return (
     <Router>
       <Header isLoggedIn={isLoggedIn} onLoginToggle={() => setIsLoggedIn(!isLoggedIn)} />
       <Routes>
-        <Route path="/" element={<Login isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn}  />} />
-        <Route path="/preliminfo" element={<PrelimInfoForm activeTab={activeTab} setActiveTab={setActiveTab}/>} />
-        <Route path="/basicinfo" element={<BasicConfigForm activeTab={activeTab} setActiveTab={setActiveTab} />} />
-        <Route path="/task" element={<TaskForm activeTab={activeTab} setActiveTab={setActiveTab} />} />
-        <Route path="/azureconfig" element={<ConfigurationsForm activeTab={activeTab} setActiveTab={setActiveTab} />} />
-        <Route path="/genartifacts" element={<GitConfigForm activeTab={activeTab} setActiveTab={setActiveTab} />} />
+        {isLoggedIn ? (
+          <>
+            <Route path="/preliminfo" element={<PrelimInfoForm activeTab={activeTab} setActiveTab={setActiveTab} />} />
+            <Route path="/basicinfo" element={<BasicConfigForm activeTab={activeTab} setActiveTab={setActiveTab} />} />
+            <Route path="/task" element={<TaskForm activeTab={activeTab} setActiveTab={setActiveTab} />} />
+            <Route path="/azureconfig" element={<ConfigurationsForm activeTab={activeTab} setActiveTab={setActiveTab} />} />
+            <Route path="/genartifacts" element={<GitConfigForm activeTab={activeTab} setActiveTab={setActiveTab} />} />
+          </>
+        ) : (
+          <>
+            <Route path="/" element={<Login isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />} />
+            <Route path="*" element={<Navigate to="/" />} />
+          </>
+        )}
       </Routes>
       <Footer />
     </Router>
